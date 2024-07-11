c843b4aba8e8c3b19fe19a7313e7672dc9487a7b
Alberto F. Borella <113259191+albertoborella@users.noreply.github.com>
2024-07-11 17:10:24 -0300

    Add files via upload

---
+++ ejercicios_practica/proyecto1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CartaPresentacion from './components/CartaPresentacion';
+
+function App() {
+  return (
+    <div className="App">
+      <CartaPresentacion />
+    </div>
+  );
+}
+
+export default App;
---
+++ ejercicios_practica/proyecto1/src/components/CartaPresentacion.jsx
@@ -0,0 +1,14 @@
+export default function CartaPresentacion() {
+
+    const mi_nombre = 'Alberto Borella'
+
+  return (
+    <div>
+        <h1>{mi_nombre}</h1>
+        <h3>Programador Backend -Python/Django- (INOVE Coding School)</h3>
+        <h4>Conocimientos HTML-CSS-JS-Bootstrap</h4>
+        <p>Pretendo adquirir los conocimentos en REACT, con la intensión de tener las herramientas básicas como para poder hacer un desarrollo completo.</p>
+        <hr/>
+    </div>
+  )
+}
